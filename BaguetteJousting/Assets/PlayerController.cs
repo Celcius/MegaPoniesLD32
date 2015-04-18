af97911c105a7aa9d8a1c937245cd061eb3aee4f
Ivo Bernardo Silva Capelo <ivo.capelo@gmail.com>
2015-04-18 14:35:27 +0100

    better movement

--- BaguetteJousting/Assets/PlayerController.cs
+++ BaguetteJousting/Assets/PlayerController.cs
@@ -4,16 +4,19 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
     const float DIR_MULT = 0.3f;
-    const float ACCEL_MOD = 0.1f;
-    const float MAX_ACCEL = 2.0f;
-    const float ROTATE_SPEED = 7.0f;
-    const float STILL_ROTATE_SPEED = 4.0f;
-    const float MAX_SPEED_ROTATE_MOD = 2.0f;
-    const float MAX_SPEED = 9.0f;
-    const float MAX_BACK_SPEED = -4.0f;
-    const float ACCEL_DECREASE = 100.0f;
+    const float ACCEL_MOD = 0.01f;
+    const float ACCEL_ROTATE_MOD = 0.05f;
+    const float MAX_ACCEL_ROTATE = 0.5f;
+    const float MAX_ACCEL = 3.0f;
+    const float ROTATE_SPEED = 170.0f;
+    const float STILL_ROTATE_SPEED = 110.5f;
+    const float MAX_SPEED_ROTATE_MOD = ROTATE_SPEED - STILL_ROTATE_SPEED;
+    const float MAX_SPEED = 15.0f;
+    const float MAX_STILL_SPEED = 5.0f;
+    const float MAX_BACK_SPEED = -10.0f;
+    const float ACCEL_DECREASE = 200.0f;
 
-
+    bool _isStill = true;
     float _acceleration = 0.0f;
     float _velocity = 0.0f;
     Transform _front;
@@ -40,14 +43,18 @@ public class PlayerController : MonoBehaviour {
             x =1.0f;
         Debug.Log(x);
 
-        if(_velocity == 0)
+        if (_isStill)
         {
-            // STILL_ROTATE_SPEED
+            transform.RotateAround(_back.position, new Vector3(0.0f, x, 0.0f), ROTATE_SPEED * Time.deltaTime);
+            if(x != 0) 
+                _acceleration = ACCEL_ROTATE_MOD;
+
+            
         }
         else
         {
-            float rotateMult = ROTATE_SPEED - MAX_SPEED_ROTATE_MOD * (Mathf.Abs(_velocity) / MAX_SPEED);
-            transform.Rotate(new Vector3(0, x * rotateMult, 0));
+            float rotateMult = (ROTATE_SPEED - STILL_ROTATE_SPEED)  * (1- Mathf.Abs(_velocity) / MAX_SPEED) + STILL_ROTATE_SPEED;
+            transform.Rotate(new Vector3(0, x * rotateMult*Time.deltaTime, 0));
 
         }
 
@@ -64,6 +71,9 @@ public class PlayerController : MonoBehaviour {
 
         bool noInput = Mathf.Abs(input) < 0.15f;
 
+        if (!noInput)
+            _isStill = false;
+
         _acceleration = _acceleration + ACCEL_MOD * -input;
         _acceleration = Mathf.Clamp(_acceleration, -MAX_ACCEL, MAX_ACCEL);
 
@@ -77,18 +87,18 @@ public class PlayerController : MonoBehaviour {
         float velSign = Mathf.Sign(_velocity);
 
         _velocity += _acceleration;
-        if(_velocity > MAX_SPEED)
-            _velocity = MAX_SPEED;
+        if(_velocity > (_isStill ? MAX_STILL_SPEED : MAX_SPEED))
+            _velocity = (_isStill ? MAX_STILL_SPEED : MAX_SPEED);
         if (_velocity < MAX_BACK_SPEED)
             _velocity = MAX_BACK_SPEED;
 
         if (velSign != Mathf.Sign(_velocity) && noInput)
         {
+            _isStill = true;
             _velocity = 0.0f;
             _acceleration = 0.0f;
         }
 
-        Debug.Log(_acceleration + " " + _velocity + " " + input);
 
         rigidbody.MovePosition(transform.position + dirVector);
 
