655cc0f8619151dd01c74238b532b9d2fad4f3b1
Ivo Bernardo Silva Capelo <ivo.capelo@gmail.com>
2015-04-18 16:56:18 +0100

    multicontrolls and trails

---
+++ BaguetteJousting/Assets/ActivateRenderer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateRenderer : MonoBehaviour {
+
+    TrailRenderer _renderer;
+    bool activated = false;
+    float decideTimer = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        _renderer = GetComponent<TrailRenderer>();
+
+        decideTimer = 1.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+	    
+	}
+}
--- BaguetteJousting/Assets/PlayerController.cs
+++ BaguetteJousting/Assets/PlayerController.cs
@@ -3,19 +3,35 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+    enum PlayerNum
+    {
+        PLAYER_ONE,
+        PLAYER_TWO,
+        PLAYER_THREE,
+        PLAYER_FOUR,
+    };
+
     const float DIR_MULT = 0.3f;
     const float ACCEL_MOD = 0.01f;
     const float ACCEL_ROTATE_MOD = 0.05f;
     const float MAX_ACCEL_ROTATE = 0.5f;
-    const float MAX_ACCEL = 3.0f;
+    const float MAX_ACCEL = 0.4f;
     const float ROTATE_SPEED = 170.0f;
     const float STILL_ROTATE_SPEED = 110.5f;
     const float MAX_SPEED_ROTATE_MOD = ROTATE_SPEED - STILL_ROTATE_SPEED;
-    const float MAX_SPEED = 15.0f;
+    const float MAX_SPEED = 30.0f;
     const float MAX_STILL_SPEED = 5.0f;
     const float MAX_BACK_SPEED = -10.0f;
-    const float ACCEL_DECREASE = 200.0f;
+    const float ACCEL_DECREASE = 1000.0f;
+    const float MAX_SCALE = 3.0f;
+    const float MIN_SCALE = 0.5f;
+    [SerializeField]
+    PlayerNum _playerNum;
 
+    [SerializeField]
+    Transform _representation;
+
+    float _baseY = 0;
     bool _isStill = true;
     float _acceleration = 0.0f;
     float _velocity = 0.0f;
@@ -25,7 +41,7 @@ public class PlayerController : MonoBehaviour {
 	void Start () {
         _front = GetComponentInChildren<FrontRef>().transform;
         _back = GetComponentInChildren<BackRef>().transform;
-	
+        _baseY = transform.position.y;
 	}
 	
 	// Update is called once per frame
@@ -35,13 +51,7 @@ public class PlayerController : MonoBehaviour {
             return;
 
 
-        
-        float x = Input.GetAxis("Horizontal");
-        if(Input.GetKey(KeyCode.LeftArrow))
-            x = -1.0f;
-        else if(Input.GetKey(KeyCode.RightArrow))
-            x =1.0f;
-        Debug.Log(x);
+        float x = getRotation();
 
         if (_isStill)
         {
@@ -62,13 +72,7 @@ public class PlayerController : MonoBehaviour {
         Vector3 dirVector = _front.position - _back.position;
         Debug.DrawLine(transform.position, transform.position + dirVector);
 
-        float input = Input.GetAxis("Move");
-       
-        if(Input.GetKey(KeyCode.UpArrow))
-            input = -1.0f;
-        else if(Input.GetKey(KeyCode.DownArrow))
-            input = 1.0f;
-
+        float input = getMovement();
         bool noInput = Mathf.Abs(input) < 0.15f;
 
         if (!noInput)
@@ -99,9 +103,59 @@ public class PlayerController : MonoBehaviour {
             _acceleration = 0.0f;
         }
 
+        float scaleVal = transform.position.y / _baseY;
+        scaleVal = Mathf.Clamp(Mathf.Pow(scaleVal, 1),MIN_SCALE,MAX_SCALE);
+        _representation.localScale = new Vector3(scaleVal,scaleVal,scaleVal);
 
         rigidbody.MovePosition(transform.position + dirVector);
-
+        
 	
 	}
+
+    float getMovement()
+    {
+        float val = 0;
+        switch (_playerNum)
+        {
+            case PlayerNum.PLAYER_ONE:
+                val= Input.GetAxis("Player1_axisMove");
+                break;
+            case PlayerNum.PLAYER_TWO:
+                val= Input.GetAxis("Player2_axisMove");
+                break;
+            case PlayerNum.PLAYER_THREE:
+                val= Input.GetAxis("Player3_axisMove");
+                break;
+            case PlayerNum.PLAYER_FOUR:
+                val= Input.GetAxis("Player4_axisMove");
+                break;
+        }
+        if (Mathf.Abs(val) < 0.8f)
+            val = 0.0f;
+        return val;
+    }
+    
+    float getRotation()
+    {
+        float val = 0;
+        switch(_playerNum)
+        {
+            case PlayerNum.PLAYER_ONE:
+                val= Input.GetAxis("Player1_axisX");
+                break;
+            case PlayerNum.PLAYER_TWO:
+                val = Input.GetAxis("Player2_axisX");
+                break;
+            case PlayerNum.PLAYER_THREE:
+                val = Input.GetAxis("Player3_axisX");
+                break;
+            case PlayerNum.PLAYER_FOUR:
+                val = Input.GetAxis("Player4_axisX");
+                break;
+        }
+        if (Mathf.Abs(val) < 0.8f)
+            val = 0.0f;
+        return val;
+    }
+        
 }
