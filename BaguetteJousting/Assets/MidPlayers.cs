668e43948d0bdad003db01240e4fddd62e9e213f
Ivo Bernardo Silva Capelo <ivo.capelo@gmail.com>
2015-04-19 22:03:33 +0100

    camera adjustments

--- BaguetteJousting/Assets/MidPlayers.cs
+++ BaguetteJousting/Assets/MidPlayers.cs
@@ -14,22 +14,27 @@ public class MidPlayers : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        float x = 0;
-        float z = 0;
+        float minX = int.MaxValue;
+        float maxX = int.MinValue;
+        float minZ = int.MaxValue;
+        float maxZ = int.MinValue;
+
         int activePlayers = 0;
 	    for(int i = 0; i < _players.Count; i++)
         {
             if(_players[i].gameObject.activeSelf)
-            { 
-             x += _players[i].transform.position.x;
-             z += _players[i].transform.position.z;
-             activePlayers++;
+            {
+                minX = Mathf.Min(_players[i].transform.position.x, minX);
+                maxX = Mathf.Max(_players[i].transform.position.x, maxX);
+                minZ = Mathf.Min(_players[i].transform.position.z, minZ);
+                maxZ = Mathf.Max(_players[i].transform.position.z, maxZ);
+                activePlayers++;
             }
         }
 
         if(activePlayers > 0)
         {
-            Vector3 destinyPos = new Vector3(Mathf.Clamp(x / activePlayers, -10, 10), transform.position.y, Mathf.Clamp(z / activePlayers, -10, 10));
+            Vector3 destinyPos = new Vector3(Mathf.Clamp( (maxX + minX)/2, -10, 10), transform.position.y, Mathf.Clamp((maxZ + minZ)/2, -10, 10));
 
              transform.position = new Vector3(transform.position.x + (destinyPos.x - transform.position.x) * Time.deltaTime*2, destinyPos.y, transform.position.z + (destinyPos.z - transform.position.z-18) * Time.deltaTime*2);
         }
